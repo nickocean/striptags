41005ebb9b35a31292a54ff5d4917d9a34b45f00
Nick Ocean <nickocean1997@gmail.com>
2018-10-23 15:33:01 +0300

    dfdg

--- HtmlTagHelper.php
+++ HtmlTagHelper.php
@@ -116,7 +116,7 @@ class HtmlTagHelper
         $config->set('Cache.SerializerPermissions', 0775);
         $config->set('Attr.EnableID', true);
         $config->set('Core.EscapeInvalidTags', true);
-	    $config->set('HTML.Allowed', 'audio');
+	    
 	    /*$config->set('HTML.DefinitionID', 'enduser-customize.html tutorial');
 	    $config->set('HTML.DefinitionRev', 1);
 	    $def = $config->getHTMLDefinition(true);
